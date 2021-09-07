309816a559cb84c109006cdd698f0222c6f9d4db
BrundashreeBS <brundashreebs@gmail.com>
2021-09-08 00:04:45 +0530

    added file

---
+++ Human.java
@@ -0,0 +1,14 @@
+class Human{
+public static void main(String a[]){
+
+   String human="A";
+   int age=55;
+   boolean isAlive=true;
+System.out.println(human);
+System.out.println(age);
+System.out.println(isAlive);
+
+
+}
+
+}
